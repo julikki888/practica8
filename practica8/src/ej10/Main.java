49efb7e0070d54dbdcb4ca49afb4970be643669f
Julikki <Julikki@192.168.1.14>
2026-01-18 14:40:01 +0100

    dfgdf

--- practica8/src/ej10/Main.java
+++ practica8/src/ej10/Main.java
@@ -1,90 +1,83 @@
 package ej10;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		
 		LibreriaObjeto lo = new LibreriaObjeto();
+		int [] array=new int [10];
 		
-		lo.cargaDatosAleatoria();
-		lo.toStringArray();
+		int menuNum, aux;
 		
-		System.out.println("\n"+lo.busquedaBinaria(30));
+		try (Scanner sc = new Scanner(System.in)){
+			do {
+			
+				menuNum=menu(sc);
+				
+				switch (menuNum) {
+				case 1 -> {lo.cargaDatos(sc); LibreriaClase.cargaDatos(sc, array);}
+				case 2 -> {lo.cargaDatosAleatoria(); LibreriaClase.cargaDatosAleatoria(array);}
+				case 3 -> {
+					System.out.println("toString metodoInstancia");
+					lo.toStringArray(); 
+					System.out.println("toString metodoClase");
+					LibreriaClase.toStringArray(array);}
+				case 4 -> {System.out.println("(MetodoInstancia)\nElemento maximo: "+lo.elementoMax()); 
+							System.out.println("\n(MetodoClase)\nElemento Maximo: "+LibreriaClase.elementoMax(array));}
+				case 5 -> {System.out.println(lo.elementoMin()); System.out.println(LibreriaClase.elementoMin(array));}
+				case 6 -> {System.out.println(lo.sumaElementos()); System.out.println(LibreriaClase.sumaElementos(array));}
+				case 7 -> {
+					System.out.println("¿Que numero desea buscar?");
+					aux = sc.nextInt();
+					System.out.println(
+							(lo.busquedaLineal(aux)<0?"No existe este numero en el array.":
+													  "El numero que buscas se encuentra en la posicion: "+lo.busquedaLineal(aux)));
+					
+				}
+				
+				default ->
+				throw new IllegalArgumentException("Unexpected value: " + menuNum);
+				}
+				
+				
+			}while(menuNum!=0); 
+				
+			
+			
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
-		lo.ordenacionBurbuja();
-		lo.toStringArray();
-		System.out.println("\n---porposicion");
-		
-		lo.borrarPorPosicion(1);
-		lo.toStringArray();
-		System.out.println();
-		
-		lo.insertarNumero(67, 4);
-		lo.insertarNumero(67, 2);
-		lo.insertarNumero(67, 3);
-		lo.toStringArray();
-		
-		
-		System.out.println();
-		lo.desordenarArray();
-		lo.toStringArray();
-		
-		
-		
-/*
- * 10.- Desarrolla dos librerías con métodos para manejar arrays:
- * 
- * --una de ellas contendrá métodos de objeto, por tanto tendremos una variable de instancia que será
-		el array con el que trabajaremos,
- * --y otra con métodos de clase, en este caso el array llegará como parámetro a los diferentes métodos.
-		Asimismo deberá desarrollar dos programas que incluyan un menú con el que poder probar las
-		librerías anteriores.
-			Los métodos que incluiremos en nuestras librerías serán:
- Carga de datos en el array mediante teclado.
-
- Carga de datos en el array de forma aleatoria.
-
- toString del array.
-
- Elemento máximo del array.
-
- Elemento mínimo del array.
-
- Suma de los elementos del array.
---------------------------------------------------------
- Métodos de búsqueda (lineal y dicotómica o binaria).
-
- Métodos varios de ordenación (búrbuja, selección, inserción y Shell).
-
- Método que desordene el array.
-
- Método de insertar por posición, es decir, colocando el nuevo número en la posición indicada,
-siempre y cuando sea válida, es decir, le abriremos un hueco en el array desplazando todos los
-elementos a la derecha y perdiendo el último elemento que haya en el array.
-
- Método de borrar por posición, es lo opuesto al método anterior, tendremos que desplazar los
-	elementos a la izquierda, y colocaremos un 0 en la última posición del array.
+	}
 	
- Método de borrar un determinado elemento (la primera ocurrencia que encontremos).
-*/
+	public static int menu(Scanner sc) {
 		
-		System.out.println("\n------------borrar elemneto0s");
-		lo.borrarDeterminado(67);
-		lo.toStringArray();
-		System.out.println("\n");
-		lo.borrarTodosDeterminados(67);
-		lo.toStringArray();
+		System.out.println("Selecciona una opcion:"
+				+ "\n0. Cerrar menu."
+				+ "\n1. Carga de datos en el array mediante teclado."
+				+ "\n2. Carga de datos en el array de forma aleatoria."
+				+ "\n3. toString del array."
+				+ "\n4. Elemento máximo del array."
+				+ "\n5. Elemento mínimo del array."
+				+ "\n6. Suma de los elementos del array."
+				+ "\n7. Método de búsqueda lineal."
+				+ "\n8. Método de búsqueda binaria."
+				+ "\n9. Método de ordenación burbuja."
+				+ "\n10. Método de ordenación seleccion."
+				+ "\n11. Método de ordenación inserccion."
+				+ "\n12. Método de ordenación Shell."
+				+ "\n13. Método de desordenacion del array."
+				+ "\n14. Insertar por posicion."
+				+ "\n15. Borrar por posicion."
+				+ "\n16. Borrar un elemento."
+				+ "\n17. Borrar un elemento completamente");
 		
-/*
- Método para borrar todas las apariciones de un elemento.
-
-
-	De forma paralela al desarrollo de las librerías se irán desarrollando unos programas para el testeo de
-las mismas, por ejemplo, un menú con las diferentes funcionalidades que ofrecen los métodos de las
-librerías.
- * 
- * 
- */
+		return sc.nextInt();
+		
 	}
 }
